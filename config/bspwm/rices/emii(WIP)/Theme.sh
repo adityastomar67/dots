0ff2bd3cb0621fba0612ae7440158ddb24c9c3ea
Aditya Singh Tomar <adityastomar67@gmail.com>
2023-07-20 12:24:07 +0530

    Added emii

---
+++ config/bspwm/rices/emii(WIP)/Theme.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+#  ╔═╗╔╦╗╦╦  ╦╔═╗  ╦═╗╦╔═╗╔═╗
+#  ║╣ ║║║║║  ║╠═╣  ╠╦╝║║  ║╣ 	https://github.com/gh0stzk/dotfiles
+#  ╚═╝╩ ╩╩╩═╝╩╩ ╩  ╩╚═╩╚═╝╚═╝
+## This file will configure the options
+## and launch the bars corresponding to each theme.
+
+# Set bspwm configuration for Emilia
+set_bspwm_config() {
+		bspc config border_width 0
+		bspc config top_padding 35
+		bspc config bottom_padding 2
+		bspc config normal_border_color "#414868"
+		bspc config active_border_color "#c0caf5"
+		bspc config focused_border_color "#bb9af7"
+		bspc config presel_feedback_color "#7aa2f7"
+		bspc config left_padding 2
+		bspc config right_padding 2
+		bspc config window_gap 6
+}
+
+# Reload terminal colors
+set_term_config() {
+		sed -i "$HOME"/.config/alacritty/fonts.yml \
+		-e "s/family: .*/family: JetBrainsMono Nerd Font/g" \
+		-e "s/size: .*/size: 10/g"
+		
+		command cat > "$HOME"/.config/alacritty/colors.yml <<- _EOF_
+				# Colors (Tokyo Night) Emilia Rice
+				colors:
+				  primary:
+				    background: '#151515'
+				    foreground: '#E8E3E3'
+
+				  normal:
+				    black:   '#151515'
+				    red:     '#B66467'
+				    green:   '#8C977D'
+				    yellow:  '#D9BC8C'
+				    blue:    '#8DA3B9'
+				    magenta: '#A988B0'
+				    cyan:    '#8AA6A2'
+				    white:   '#E8E3E3'
+
+				  bright:
+				    black:   '#424242'
+				    red:     '#B66467'
+				    green:   '#8C977D'
+				    yellow:  '#D9BC8C'
+				    blue:    '#8DA3B9'
+				    magenta: '#A988B0'
+				    cyan:    '#8AA6A2'
+				    white:   '#E8E3E3'
+
+				  cursor:
+				    cursor: '#bb9af7'
+				    text:	'#1a1b26'
+_EOF_
+}
+
+# Reload xresources colors
+set_xresources_config() {
+		command cat > "$HOME"/.config/bspwm/xresources <<- _EOF_
+				Xft.antialias:	  1
+				Xft.hinting:	  1
+				Xft.autohint:	  0
+				Xft.hintstyle:	  hintslight
+				Xft.rgba:	      rgb
+				Xft.lcdfilter:	  lcddefault
+
+				! font
+				st.font:          Operator Mono Lig:style:medium:pixelsize=18
+				dmenu.font:       Operator Mono Lig:style:medium:pixelsize=18
+
+				! window-padding
+				st.borderpx:      20
+
+				! transparency
+				st.alpha:         0.9
+
+				! special
+				*.foreground:     #E8E3E3
+				*.background:     #151515
+
+				! black
+				*.color0:         #151515
+				*.color8:         #424242
+
+				! red
+				*.color1:         #B66467
+				*.color9:         #B66467
+
+				! green
+				*.color2:         #8C977D
+				*.color10:        #8C977D
+
+				! yellow
+				*.color3:         #D9BC8C
+				*.color11:        #D9BC8C
+
+				! blue
+				*.color4:         #8DA3B9
+				*.color12:        #8DA3B9
+
+				! magenta
+				*.color5:         #A988B0
+				*.color13:        #A988B0
+
+				! cyan
+				*.color6:         #8AA6A2
+				*.color14:        #8AA6A2
+
+				! white
+				*.color7:         #E8E3E3
+				*.color15:        #E8E3E3
+_EOF_
+}
+
+# Set compositor configuration
+set_picom_config() {
+		sed -i "$HOME"/.config/bspwm/picom.conf \
+			-e "s/shadow-color = .*/shadow-color = \"#000000\"/g" \
+			-e "s/corner-radius = .*/corner-radius = 6/g" \
+			-e "s/\".*:class_g = 'Alacritty'\"/\"100:class_g = 'Alacritty'\"/g" \
+			-e "s/\".*:class_g = 'FloaTerm'\"/\"100:class_g = 'FloaTerm'\"/g" \
+			-e "s/\".*:class_g = 'Updating'\"/\"100:class_g = 'Updating'\"/g" \
+			-e "s/\".*:class_g = 'MusicPlayer'\"/\"100:class_g = 'MusicPlayer'\"/g" \
+			-e "s/\".*:class_g = 'Sysfetch'\"/\"100:class_g = 'Sysfetch'\"/g" \
+			-e "s/\".*:class_g = 'scratch'\"/\"90:class_g = 'scratch'\"/g"
+}
+
+# Set dunst notification daemon config
+set_dunst_config() {
+		sed -i "$HOME"/.config/bspwm/dunstrc \
+		-e "s/transparency = .*/transparency = 0/g" \
+		-e "s/frame_color = .*/frame_color = \"#1a1b26\"/g" \
+		-e "s/separator_color = .*/separator_color = \"#c0caf5\"/g" \
+		-e "s/font = .*/font = JetBrainsMono Nerd Font Medium 9/g" \
+		-e "s/foreground='.*'/foreground='#f9f9f9'/g"
+		
+		sed -i '/urgency_low/Q' "$HOME"/.config/bspwm/dunstrc
+		cat >> "$HOME"/.config/bspwm/dunstrc <<- _EOF_
+				[urgency_low]
+				timeout = 3
+				background = "#1a1b26"
+				foreground = "#c0caf5"
+
+				[urgency_normal]
+				timeout = 6
+				background = "#1a1b26"
+				foreground = "#c0caf5"
+
+				[urgency_critical]
+				timeout = 0
+				background = "#1a1b26"
+				foreground = "#c0caf5"
+_EOF_
+}
+
+# Launch the bar
+launch_bars() {
+		polybar -q emi-bar -c ${rice_dir}/config.ini &
+}
+
+
+### ---------- Apply Configurations ---------- ###
+
+set_bspwm_config
+set_term_config
+set_picom_config
+set_dunst_config
+launch_bars
